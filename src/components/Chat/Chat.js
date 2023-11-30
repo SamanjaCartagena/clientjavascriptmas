acaeebc786375bdca7570ee41e902dd6e96b9428
SamanjaCartagena <cart.samanja09@gmail.com>
2023-11-30 02:26:34 -0500

    first commit

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,10 @@
+import React,{useState,useEffect} from "react";
+import queryString from 'query-string'
+import io from 'socket.io-client'
+function Chat() {
+  return <div><h1>Chat</h1></div>;
+}
+
+export default Chat;
+
+
